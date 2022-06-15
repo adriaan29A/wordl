581b60fecfdb01b70a8a56ad540052d367de6875
Adriaan Canter <adriaan.canter@gmail.com>
2022-06-15 08:46:57 -0700

    go

--- base/scratch.py
+++ base/scratch.py
@@ -1,2 +1,38 @@
-import wordl
+"""
+class Foo:
 
+    def fribble(x):
+        print(x)
+
+    def frooble(x):
+        Foo.fribble(x)
+
+
+Foo.frooble(10)
+
+"""
+
+class MyClass:
+
+    """A simple example class"""
+    i = 12345
+
+    @staticmethod
+    def rc():
+        return 'goodbye world'
+
+    @staticmethod
+    def f(bar):
+        print(bar)
+        print("hello world")
+        print(MyClass.rc())
+
+
+
+c = MyClass()
+
+mf = MyClass.f("bar")
+print(mf)
+
+mf = c.f('bar')
+print(mf)
